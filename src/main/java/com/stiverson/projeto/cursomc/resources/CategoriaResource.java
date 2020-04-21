cfc15baef233e2057dd776719cb6b278cc8a7510
Stiverson <Stiverson.machado@gmail.com>
2020-04-21 20:23:25 -0300

    Testando primeira classe de dominio Categoria

--- src/main/java/com/stiverson/projeto/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/stiverson/projeto/cursomc/resources/CategoriaResource.java
@@ -1,17 +1,29 @@
 package com.stiverson.projeto.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.stiverson.projeto.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
+	public List<Categoria> listar() {
 		
-		return "Rest ta funfando cara ^^";
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Games");
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+	
+		return lista;
 		
 	}
 }
