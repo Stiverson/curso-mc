e4bd5b49f66777ba35fa77fdadb6ca5ff2825a1e
Stiverson <Stiverson.machado@gmail.com>
2020-04-22 13:07:11 -0300

    Criação operação de instanciação

--- src/main/java/com/stiverson/projeto/cursomc/CursoMcApplication.java
+++ src/main/java/com/stiverson/projeto/cursomc/CursoMcApplication.java
@@ -1,13 +1,32 @@
 package com.stiverson.projeto.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.stiverson.projeto.cursomc.domain.Categoria;
+import com.stiverson.projeto.cursomc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null,"Informática");
+		Categoria cat2 = new Categoria(null,"Games");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
