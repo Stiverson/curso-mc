b852b4cd3d9e2ea84b675e6ebcb93de4b9cffe87
Stiverson <Stiverson.machado@gmail.com>
2020-04-22 15:12:07 -0300

    Ajustes no endpoint /categorias/{id} quando o meesmo não existir

--- src/main/java/com/stiverson/projeto/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/stiverson/projeto/cursomc/resources/CategoriaResource.java
@@ -1,7 +1,6 @@
 package com.stiverson.projeto.cursomc.resources;
 
-import java.util.ArrayList;
-import java.util.List;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -21,13 +20,8 @@ public class CategoriaResource {
 	private CategoriaService service;
 	
 	@RequestMapping(value="/{id}",method=RequestMethod.GET)
-	public ResponseEntity<?> find(@PathVariable Integer id) {
+	public ResponseEntity<?> find(@PathVariable Integer id){
 		Categoria obj = service.find(id);
-		return ResponseEntity.ok(obj);
-		
-		
-		
-		
-		
+		return ResponseEntity.ok().body(obj);
 	}
 }
